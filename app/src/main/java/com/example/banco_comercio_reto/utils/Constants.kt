727cdd31d1420f78028db45ed77ffbe509dab558
Ernesto Chira Torres <echira@overall.com.pe>
2021-03-23 07:44:51 -0500

    modifications

--- app/src/main/java/com/example/banco_comercio_reto/utils/Constants.kt
+++ app/src/main/java/com/example/banco_comercio_reto/utils/Constants.kt
@@ -1,8 +1,10 @@
 package com.example.banco_comercio_reto.utils
 
+import com.example.banco_comercio_reto.BuildConfig
+
 class Constants {
     companion object {
-        const val BASE_URL = "https://jsonplaceholder.typicode.com"
+        const val BASE_URL = BuildConfig.BASE_URL
         const val REQUEST_TIMEOUT_DURATION = 10
     }
 }
